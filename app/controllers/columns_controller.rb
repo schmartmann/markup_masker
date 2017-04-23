80b99bcf53b879ebab0f88b2eb172449d884ad34
Stefan Hartmann <stefanhartmann@gmail.com>
2017-04-23 01:30:02 -0400

    start building out HTML filtering method

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,6 +1,10 @@
 class ColumnsController < ApplicationController
+  helper ColumnsHelper
   def show
     column = Column.where(id: params[:id]).joins(:rows)
     @column = column[0]
+    @column.rows.each do |row|
+      Row.html_breaker(row)
+    end
   end
 end
