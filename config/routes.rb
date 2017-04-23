ed75884f687555fd1aaf2b81a7c22a12cdf8cdf5
Stefan Hartmann <stefanhartmann@gmail.com>
2017-04-22 23:33:54 -0400

    add devise views / controllers / routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :excel_sheets do 
+  resources :excel_sheets do
     resources :sheets do
-      resources :columns do 
+      resources :columns do
         resources :rows
       end
     end
