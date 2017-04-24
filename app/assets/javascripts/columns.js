3956369fae7d13580b962a33d4ebfdd3fae80092
Stefan Hartmann <stefanhartmann@gmail.com>
2017-04-24 14:04:53 -0400

    restyle columns show

---
+++ app/assets/javascripts/columns.js
@@ -0,0 +1,9 @@
+debugger
+console.log("hact");
+const code = $("code");
+
+const rowNumbers = document.getElementsByClassName("row-number"); 
+const htmlShow = document.getElementById("html_show");
+const htmlHide = document.getElementById("html_hidden");
+const rowShow = document.getElementById("row_show")
+const rowHide = document.getElementById("row_hidden");
