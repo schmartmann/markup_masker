bc7bb200485c941539ef61bad6693fd03540e761
Stefan Hartmann <stefanhartmann@gmail.com>
2017-04-24 14:27:19 -0400

    add JS to hit MVP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,53 @@
 //= require jquery_ujs
 //= require turbolinks
 // = require_tree .
-// = stub columns
+$(document).ready(function(){
 
+  const html = $(".code");
+  const rowNumber = $(".row-number");
+  const htmlShow = $("#html_show");
+  const htmlHide = $("#html_hidden");
+  const rowShow = $("#row_show");
+  const rowHide = $("#row_hidden"); 
 
+  const hideElement = (name) => {
+    if (name === "html"){
+      html.hide()
+    } else {
+      rowNumber.hide()
+    }
+  }
+
+  const showElement = (name) => {
+    if (name === "html"){
+      html.show()
+    } else {
+      rowNumber.show()
+    }
+  }
+
+  const valCheck = (target) => {
+    if (target.value === "show") {
+      showElement(target.name);
+    } else {
+      hideElement(target.name);
+    }
+  }
+
+  const clickHandler = (target) => {
+    valCheck(target);
+  }
+
+  htmlShow.click(function(){
+    clickHandler(event.target);
+  });
+  htmlHide.click(function(){
+    clickHandler(event.target);
+  });
+  rowShow.click(function(){
+    clickHandler(event.target);
+  });
+  rowHide.click(function(){
+    clickHandler(event.target);
+  }); 
+});
--- app/assets/javascripts/columns.js
+++
@@ -1,9 +0,0 @@
-debugger
-console.log("hact");
-const code = $("code");
-
-const rowNumbers = document.getElementsByClassName("row-number"); 
-const htmlShow = document.getElementById("html_show");
-const htmlHide = document.getElementById("html_hidden");
-const rowShow = document.getElementById("row_show")
-const rowHide = document.getElementById("row_hidden");
